c06433fe1a17511f479efb0c12ec3f8bfeb1e1ee
ClarabelleCheng-Yue <cachengyue@gmail.com>
2017-01-06 14:40:56 -0800

    Make book list item clickable and add state to search bar

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -1,48 +1,34 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchWeather } from '../actions/index';
 
-class SearchBar extends Component {
+export default class SearchBar extends Component {
+
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
-
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.state = {term: ''};
+    this.obj = {};
   }
 
   onInputChange(event) {
-    this.setState({ term: event.target.value });
-  }
-
-  onFormSubmit(event) {
-    event.preventDefault();
-
-    // We need to go and fetch weather data
-    this.props.fetchWeather(this.state.term);
-    this.setState({ term: '' });
+    // console.log('event, this: ', event.target.value, this);
+    console.log('event, this: ', event, this);
+    // this.setState({ term: event.target.value });
+    this.setState({ term: event });
   }
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
-        <input
-          placeholder="Get a five-day forecast in your favorite cities"
+      <form className="input-group">
+        <input 
+          placeholder="Enter a city"
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange} />
+          // onChange={this.onInputChange} />
+          onChange={event => this.onInputChange(event.target.value)} />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Submit</button>
+          <button type="submit" className="btn btn-secondary">Weather me!</button>
         </span>
       </form>
     );
   }
 }
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchWeather }, dispatch);
-}
-
-export default connect(null, mapDispatchToProps)(SearchBar);
