8e294400648868f426fcdfe949955bab82f379a8
ClarabelleCheng-Yue <cachengyue@gmail.com>
2017-01-06 16:48:22 -0800

    Add json file

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
-
-const API_KEY = '6a78596d062df78380eff5944c4e5567';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const API_KEY = '609dc4bd882d0bc17701a7143c0dedee';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+// action creators:
 export function fetchWeather(city) {
+  // search query
   const url = `${ROOT_URL}&q=${city},us`;
+  // make a get request on the url, returns a promise
   const request = axios.get(url);
-
   return {
     type: FETCH_WEATHER,
     payload: request
--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
-export default class SearchBar extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
@@ -16,9 +18,20 @@ export default class SearchBar extends Component {
     this.setState({ term: event });
   }
 
+  onFormSubmit(event) {
+    // don't submit the form so page doesnt refresh
+    event.preventDefault();
+
+    // fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form 
+        onSubmit={this.onFormSubmit.bind(this)}
+        className="input-group">
         <input 
           placeholder="Enter a city"
           className="form-control"
@@ -32,3 +45,11 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+// hook up action creator fetchweather to searchbar container
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// actions always second argument. don't need to pass in state props in first param
+export default connect(null, mapDispatchToProps)(SearchBar);
