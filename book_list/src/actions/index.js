2f4aa5b23677ad249936f7f5625868e46dafc983
ClarabelleCheng-Yue <cachengyue@gmail.com>
2017-01-06 16:47:22 -0800

    Add json file

--- book_list/src/actions/index.js
+++ book_list/src/actions/index.js
@@ -1,8 +1,9 @@
 export function selectBook(book) {
-  // selectBook is an ActionCreator, it needs to return an action,
-  // an object with a type property.
+  // selectbook is an action creator, it needs to return an action,
+  // an object with a type property and sometimes a payload.
+  // console.log('A has been selected: ', book.title);
   return {
     type: 'BOOK_SELECTED',
     payload: book
-  };
+  }
 }
--- book_list/src/containers/book-list.js
+++ book_list/src/containers/book-list.js
@@ -4,11 +4,12 @@ import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
+
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li
-          key={book.title}
+        <li 
+          key={ book.title } 
           onClick={() => this.props.selectBook(book)}
           className="list-group-item">
           {book.title}
@@ -20,29 +21,33 @@ class BookList extends Component {
   render() {
     return (
       <ul className="list-group col-sm-4">
-        {this.renderList()}
+        {
+          this.renderList()
+        }
       </ul>
-    )
+    );
   }
 }
 
+// connect redux state to this container
 function mapStateToProps(state) {
-  // Whatever is returned will show up as props
-  // inside of BookList
+  // whatever is returned here will show up as this.props inside of BookList
+  // if state changes, container will auto rerender
   return {
     books: state.books
   };
 }
 
-// Anything returned from this function will end up as props
-// on the BookList container
+// connect action creator to this container
+// anything returned from this fn will end up as props on the booklist container
+// name of prop is prop in arg object
 function mapDispatchToProps(dispatch) {
-  // Whenever selectBook is called, the result shoudl be passed
-  // to all of our reducers
+  // whenever selectBook is called, 
+  // the result should be passed to all of our reducers
   return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
-// Promote BookList from a component to a container - it needs to know
-// about this new dispatch method, selectBook. Make it available
-// as a prop.
+// promote booklist from a componenet to a container,
+// it needs to know about dispatch method, selectbook. 
+// Make it available as a prop
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
