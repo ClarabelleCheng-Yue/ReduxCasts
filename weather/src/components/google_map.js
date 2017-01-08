fce24074fa1d2fa0d0bdcce4ecd7751bbc526a5e
ClarabelleCheng-Yue <cachengyue@gmail.com>
2017-01-07 18:10:37 -0800

    Add google maps to table

--- weather/src/components/google_map.js
+++ weather/src/components/google_map.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 
-export default (props) => {
+export default ({lat, lon}) => {
+  const location = { lat, lng: lon };
+
   return (
     <GoogleMapLoader
-      containerElement={ <div style={{height: '100%'}} /> }
+      containerElement={ <div style={ { height: '100%' } } /> }
       googleMapElement={
-        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
+        <GoogleMap 
+          defaultZoom={10} 
+          defaultCenter={location}>
+          <Marker 
+            position={location} 
+            defaultAnimation={3} />
+        </GoogleMap>
       }
-      />
+    />
   );
 }
